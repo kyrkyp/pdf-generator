2a52a33af921c1be6610fd92d5f79456ebb251b3
Kyriakos Kypraios <109945212+kyrkyp@users.noreply.github.com>
2025-06-14 05:38:45 +0300

    Initial

---
+++ main.py
@@ -0,0 +1,19 @@
+from fpdf import FPDF
+import pandas as pd
+
+
+pdf = FPDF(orientation='P', unit='mm', format='A4')
+
+df = pd.read_csv("topics.csv")
+
+for index, row in df.iterrows():
+    pdf.add_page()
+    pdf.set_font("arial", size=24, style='B')
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(0, 12, txt=row['Topic'], ln=1, align='C')
+    pdf.line(10, 21, 200, 21)
+
+    for i in range(row["Pages"] - 1):
+        pdf.add_page()
+
+pdf.output("demo.pdf")
